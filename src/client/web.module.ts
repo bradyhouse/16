478024f72aea927ecea7f8cd6ee87204adb88572
bradyhouse <bradyhouse@gmail.com>
2017-09-14 19:10:38 -0500

    201708290420 - closes issues #6, closes issues #8

--- src/client/web.module.ts
+++ src/client/web.module.ts
@@ -1,21 +1,19 @@
-// angular
 import { NgModule } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { Http } from '@angular/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-// libs
+
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { TranslateLoader } from '@ngx-translate/core';
 
-// app
 import { APP_COMPONENTS, AppComponent } from './app/components/index';
 import { routes } from './app/components/app.routes';
 
-// feature modules
 import { WindowService, StorageService, ConsoleService, createConsoleTarget, provideConsoleTarget, LogTarget, LogLevel, ConsoleTarget } from './app/modules/core/services/index';
 import { CoreModule, Config } from './app/modules/core/index';
 import { AnalyticsModule } from './app/modules/analytics/index';
@@ -23,10 +21,8 @@ import { MultilingualModule, Languages, translateLoaderFactory, MultilingualEffe
 import { SampleModule, SampleEffects } from './app/modules/sample/index';
 import { AppReducer } from './app/modules/ngrx/index';
 
-// config
 Config.PLATFORM_TARGET = Config.PLATFORMS.WEB;
 if (String('<%= BUILD_TYPE %>') === 'dev') {
-  // only output console logging in dev mode
   Config.DEBUG.LEVEL_4 = true;
 }
 
@@ -34,13 +30,11 @@ let routerModule = RouterModule.forRoot(routes);
 
 if (String('<%= TARGET_DESKTOP %>') === 'true') {
   Config.PLATFORM_TARGET = Config.PLATFORMS.DESKTOP;
-  // desktop (electron) must use hash
   routerModule = RouterModule.forRoot(routes, { useHash: true });
 }
 
 declare var window, console, localStorage;
 
-// For AoT compilation to work:
 export function win() {
   return window;
 }
@@ -99,6 +93,10 @@ if (String('<%= BUILD_TYPE %>') === 'dev') {
     {
       provide: Languages,
       useValue: Config.GET_SUPPORTED_LANGUAGES()
+    },
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
     }
   ],
   bootstrap: [AppComponent]
