5a79655da655e95e873f94c9bd0448e005614a21
bradyhouse <bradyhouse@gmail.com>
2017-09-14 19:36:43 -0500

    201708290420 - closes issues #6, closes issues #8

--- src/client/app/components/home/home.routes.ts
+++ src/client/app/components/home/home.routes.ts
@@ -3,6 +3,10 @@ import { HomeComponent } from './home.component';
 export const HomeRoutes: Array<any> = [
   {
     path: '',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
     component: HomeComponent
   }
 ];
--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -111,7 +111,7 @@ export class SeedConfig {
    * which can be overriden by the `--base` flag when running `npm start`.
    * @type {string}
    */
-  APP_BASE = argv['base'] || '/';
+  APP_BASE = argv['base'] || '/16/';
 
   /**
    * The base path of node modules.
